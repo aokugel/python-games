70ac9f1975500101712b1ac38b1aaa9b90bf417e
Anthony Kugel <akugel@ntst.com>
2020-08-28 19:05:44 -0500

    cleaned up the code

--- snake.py
+++ snake.py
@@ -3,15 +3,16 @@ import random
 
 pygame.init()
 
-game_width = 500
-game_height = 500
-snakecolor = (255, 153, 51)
-foodcolor = (51, 153, 255)
-win = pygame.display.set_mode((game_width,game_height))
+GAME_WIDTH = 500
+GAME_HEIGHT = 500
+SNAKECOLOR = (255, 153, 51)
+FOODCOLOR = (51, 153, 255)
+SNAKEBLOCK = 20
+
+win = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
 pygame.display.set_caption("Anthony Kugel Snake Game")
-x = 20
-y = 20 
-snakeblock = 20
+x = 40
+y = 40 
 vel = 20
 clock = pygame.time.Clock()
 run = True
@@ -23,16 +24,22 @@ score = 0
 
 
 
-def message(msg, color):
+def message(msg, color, x, y):
     mesg = font.render(msg, True, color)
-    win.blit(mesg, [int(game_width/3), int(game_height/2)])
+    win.blit(mesg, [int(x), int(y)])
 
 class Snake:
     def __init__(self, body):
         self.body = body
+        self.x = x
+        self.y = y
+        self.size = SNAKEBLOCK
+        self.xDelta = 0
+        self.yDelta = 0
+
     def draw(self):
         for x in self.body:
-            pygame.draw.rect(win, snakecolor, [x[0], x[1], snakeblock, snakeblock])
+            pygame.draw.rect(win, SNAKECOLOR, [x[0], x[1], self.size, self.size])
     def grow(self, x, y):
         self.body.append([x, y])
     def slither(self, x, y):
@@ -41,18 +48,18 @@ class Snake:
         
 class Food:
     def __init__(self, food_x=0, food_y=0):
-        self.size = snakeblock
+        self.size = SNAKEBLOCK
         self.location_x = food_x
         self.location_y = food_y
     def newfood(self):
-        randX = random.randint(0, game_width-snakeblock)
-        randXModulus = randX % snakeblock
+        randX = random.randint(0, GAME_WIDTH-self.size)
+        randXModulus = randX % SNAKEBLOCK
         self.location_x = randX - randXModulus
-        randY = random.randint(0, game_height-snakeblock)
-        randYModulus = randY % snakeblock
+        randY = random.randint(0, GAME_HEIGHT-self.size)
+        randYModulus = randY % SNAKEBLOCK
         self.location_y = randY - randYModulus
     def drawFood(self):
-        pygame.draw.rect(win, foodcolor, [self.location_x, self.location_y, self.size, self.size])
+        pygame.draw.rect(win, FOODCOLOR, [self.location_x, self.location_y, self.size, self.size])
 
 food = Food()
 food.newfood()
@@ -68,27 +75,33 @@ while run:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT]:
-        if xDelta != vel:
-            xDelta = -(vel)
-            yDelta = 0
+        if snake.xDelta != vel:
+            snake.xDelta = -(vel)
+            snake.yDelta = 0
     if keys[pygame.K_RIGHT]:
-        if xDelta != -(vel):
-            xDelta = vel
-            yDelta = 0
+        if snake.xDelta != -(vel):
+            snake.xDelta = vel
+            snake.yDelta = 0
     if keys[pygame.K_UP]:
-        if yDelta != vel:
-            xDelta = 0
-            yDelta = -(vel)
+        if snake.yDelta != vel:
+            snake.xDelta = 0
+            snake.yDelta = -(vel)
     if keys[pygame.K_DOWN]:
-        if yDelta != -(vel):
-            xDelta = 0
-            yDelta = vel
-    x += xDelta
-    y += yDelta
+        if snake.yDelta != -(vel):
+            snake.xDelta = 0
+            snake.yDelta = vel
+    x += snake.xDelta
+    y += snake.yDelta
 
-    if x > game_width or x < 0 or y > game_height or y < 0:
+    if x > GAME_WIDTH or x < 0 or y > GAME_HEIGHT or y < 0:
         lost = True
         run = False
+        break
+
+    if snake.body[0] in snake.body[1:] and score > 0:
+        lost = True
+        run = False
+        break
 
     if food.location_x == x and food.location_y == y:
         food.newfood()
@@ -100,13 +113,14 @@ while run:
     win.fill((0,0,0))
     snake.draw()
     food.drawFood()
+    message(f"{score}", (255, 0, 0), 10, 10)
     pygame.display.update()
 
 
     clock.tick(10)
 
 if lost:
-    message(f"You suck! Your score is {score}", (255, 0, 0))
+    message(f"You lose! Your score is {score}", (255, 0, 0), GAME_WIDTH/4, GAME_HEIGHT/2)
     pygame.display.update()
 
     pygame.time.wait(2000)
