29145eb3fd2ce02afbcadbb41fcf5f28d52e7bb7
Anthony Kugel <akugel@ntst.com>
2020-09-10 18:14:53 -0500

    fixed the text version of the sudoku solver

--- sudoku.py
+++ sudoku.py
@@ -1,28 +1,23 @@
-class Solution:
-    def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
+class Sudoku:
+    def __init__(self, board):
         self.board = board
-        self.solve()
         
     def solve(self):
         row, col = self.find_empty()
         if row == -1 or col == -1:
             return True
         for number in range(1, 10):
-            if self.isValid(row, col, str(number)):
-                self.board[row][col] = str(number)
+            if self.isValid(row, col, number):
+                self.board[row][col] = number
                 if self.solve():
                     return True
-                self.board[row][col]= "."
+                self.board[row][col]= 0
                 
-        
     def find_empty(self):
         for row in range(len(self.board)):
             for col in range(len(self.board[row])):
-                if self.board[row][col] == ".":
-                    return (row, col) # row, col
+                if self.board[row][col] == 0:
+                    return (row, col)
         return (-1, -1)
     
     def isValid(self, row, column, number):
@@ -32,20 +27,37 @@ class Solution:
             return False
     
     def isRowValid(self, row, number):
-        if str(number) in self.board[row]:
+        if number in self.board[row]:
             return False
         else:
             return True
         
     def isColumnValid(self, column, number):
-        if str(number) in [row[column] for row in self.board]:
+        if number in [row[column] for row in self.board]:
             return False
         else:
             return True
         
     def isSubgridValid(self, row, column, number):
         startRow, startCol = (row//3)*3, (column//3)*3
-        if str(number) in [self.board[i][j] for i in range(startRow, startRow+3) for j in range(startCol, startCol+3)]:
+        if number in [self.board[i][j] for i in range(startRow, startRow+3) for j in range(startCol, startCol+3)]:
             return False
         else:
             return True
+
+print("Enter your sudoku board as a two dimensional lists:")
+
+board = []
+while True:
+    row = list(input().split())
+    if row:
+        board.append([int(x) for x in row])
+    else:
+        break
+
+sudoku = Sudoku(board)
+sudoku.solve()
+print("This is your solved sudoku board:")
+
+for row in sudoku.board:
+    print(row)
